d44cc408d0a67de7972cdde5cc9e2b0684d0d93e
martinaliporata <martinaliporata@gmail.com>
2024-06-27 22:35:03 +0200

    implementing milestone 3

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
             return $hotel['parking'] === true;
         });
     }
+// Se è settato effettivamente il valore della get al posto di vote, se vote è maggiore di 1 e minore o uguale a 5, allora usa lui, altrimenti vote è una stringa vuota.
+    $vote = isset($_GET['vote']) && $_GET['vote'] >= 1 && $_GET['vote'] <= 5 ? $_GET['vote'] : '';
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +23,11 @@
         <form action="./index.php" method="get">
             <label for="parking">Do you need a parking?</label>
             <input type="checkbox" id="parking" name="parking" value="1" <?php echo isset($_GET['parking']) ? 'checked' : ''; ?>>
+            <button type="submit">Filtra</button>
+        </form>
+        <form action="./index.php" method="get">
+            <label for="vote">How many stars should the hotel have?</label>
+            <input type="text" name="vote" id="vote">
             <button type="submit">Filtra</button>
         </form>
         <ul>
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
-Milestone 3 
+Descrizione
+Milestone 2
+Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+
+Milestone 3
 Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
 
 NOTA:
-deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e
-che hanno un voto di tre stelle o superiore) Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
+deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore) Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
