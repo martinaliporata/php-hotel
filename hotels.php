9f6d111230bb3f9ed2c80425953a4eca81c84732
martinaliporata <martinaliporata@gmail.com>
2024-07-01 18:57:17 +0200

    correct milestone 2 and added comments

--- hotels.php
+++ hotels.php
@@ -36,4 +36,110 @@
             'distance_to_center' => 50
         ],
     ];
+
+
+    // è giusto, lo commento perché viene richiesto di mostrarlo in tabella
+    // doppio foreach
+    // le "" servono per inserire dell'html in php
+// foreach($hotels as $hotel) {
+//     echo "<h1> HOTEL </h1>";
+//     // per ogni hotel stampami chiave e valore
+//     foreach($hotel as $key => $value) {
+//         echo "$key --- $value";
+//         echo "<br>";
+//     }
+// }
+
+
+
+//Mi creo un nuovo array su cui lavorare. Questa è una copia shallow o deep? Se modifico uno si modifica pure l'altro. Questa è una copia deep.
+// Lo chiamo filtered hotels perché lo filtro
+$filteredHotels= $hotels;
+// se è settato questo valore che io sto andando a visluaizzare, allora lo uso quindi filtro.
+if (isset($_GET["parking"])){
+    $parking=($_GET["parking"]);
+    // ho selezionato per filtrare gli hotel con parcheggio perché yes ha value 1
+    if ($parking == 1){
+        // allora salvami in un temporary array gli hotel filtrati
+        $tempArray=[];
+        // per ogni hotel dell'array filtered hotels, se il valore della chiave parking è true...
+        foreach($filteredHotels as $hotel) {
+            if($hotel["parking"]===true) {
+                // allora pushami l'hotel nel nuovo array
+                $tempArray[]= $hotel;
+            }  
+        }
+        // quindi filtered array sarà uguale a temporray array
+        $filteredHotels = $tempArray;
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Hotel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <main class="container-fluid">
+        <div class="row">
+            <form action="./hotels.php" method="get">
+                <label for="parking">Do you need a parking?</label>
+                <select name="parking" id="parking">
+                    <!-- di base il no è quello selezionato -->
+                    <option value="0" selected>Not</option>
+                    <option value="1">Yes</option>
+                </select>
+                <button type="submit">Filtra</button>
+            </form>
+        </div>
+        <div class="row">
+            <form action="./index.php" method="get">
+                <label for="vote">How many stars should the hotel have?</label>
+                <input type="text" name="vote" id="vote">
+                <button type="submit">Filtra</button>
+            </form>
+        </div>
+        <div class="row">
+            <table class="col-12">
+                <thead>
+                    <!-- questi elementi non devono essere dinamici -->
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Description
+                        </th>
+                        <th>
+                            Parking
+                        </th>
+                        <th>
+                            Vote
+                        </th>
+                        <th>
+                            Distance to center
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- il tr va istanziato per ogni hotel -->
+                    <?php foreach($hotels as $hotel) { ?>
+                        <tr>
+                            <!-- per ogni proprietà di un hotel -->
+                            <?php foreach ($hotel as $property) { ?>
+                                <td>
+                                    <?php echo "$property" ?>
+                                </td>
+                            <?php } ?>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -16,13 +16,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-Hotel</title>
 </head>
 <body>
     <main>
         <form action="./index.php" method="get">
             <label for="parking">Do you need a parking?</label>
-            <input type="checkbox" id="parking" name="parking" value="1" <?php echo isset($_GET['parking']) ? 'checked' : ''; ?>>
+            <select name="" id="">Yes</select>
+            <select name="" id="">Not</select>
             <button type="submit">Filtra</button>
         </form>
         <form action="./index.php" method="get">
@@ -30,15 +30,6 @@
             <input type="text" name="vote" id="vote">
             <button type="submit">Filtra</button>
         </form>
-        <ul>
-            <?php foreach ($hotels as $hotel) { ?>
-                <h2>Nome: <?php echo $hotel['name'] ?></h2>
-                <p> Descrizione: <?php echo $hotel['description'] ?></p>
-                <p>Parcheggio: <?php echo ($hotel['parking'] ? 'Sì' : 'No')?></p>
-                <p>Voto: <?php echo $hotel['vote'] ?></p>
-                <p>Distanza dal centro città:<?php echo $hotel['distance_to_center'] ?></p>
-            <?php } ?>
-        </ul>
     </main>
 </body>
 </html>
