4638b0c27332aae7d87f674672c63ae36e932183
martinaliporata <martinaliporata@gmail.com>
2024-07-01 20:42:19 +0200

    coorect adding comments

--- hotels.php
+++ hotels.php
@@ -55,7 +55,7 @@
 //Mi creo un nuovo array su cui lavorare. Questa è una copia shallow o deep? Se modifico uno si modifica pure l'altro. Questa è una copia deep.
 // Lo chiamo filtered hotels perché lo filtro
 $filteredHotels= $hotels;
-// se è settato questo valore che io sto andando a visluaizzare, allora lo uso quindi filtro.
+// se è settato questo valore che io sto andando a visualizzare, allora lo uso quindi lo filtro.
 if (isset($_GET["parking"])){
     $parking=($_GET["parking"]);
     // ho selezionato per filtrare gli hotel con parcheggio perché yes ha value 1
@@ -65,6 +65,24 @@ if (isset($_GET["parking"])){
         // per ogni hotel dell'array filtered hotels, se il valore della chiave parking è true...
         foreach($filteredHotels as $hotel) {
             if($hotel["parking"]===true) {
+                // allora pushami l'hotel nel nuovo array
+                $tempArray[]= $hotel;
+            }  
+        }
+        // quindi filtered array sarà uguale a temporray array
+        $filteredHotels = $tempArray;
+    }
+}
+
+if (isset($_GET["stars"])){
+    $stars=($_GET["stars"]);
+    // ho selezionato per filtrare gli hotel con parcheggio perché yes ha value 1
+    if ($stars >= 1 && $stars <= 5){
+        // allora salvami in un temporary array gli hotel filtrati
+        $tempArray=[];
+        // per ogni hotel dell'array filtered hotels, se il valore della chiave parking è true...
+        foreach($filteredHotels as $hotel) {
+            if($hotel["vote"] >= $stars) {
                 // allora pushami l'hotel nel nuovo array
                 $tempArray[]= $hotel;
             }  
@@ -87,20 +105,21 @@ if (isset($_GET["parking"])){
     <main class="container-fluid">
         <div class="row">
             <form action="./hotels.php" method="get">
-                <label for="parking">Do you need a parking?</label>
-                <select name="parking" id="parking">
-                    <!-- di base il no è quello selezionato -->
-                    <option value="0" selected>Not</option>
-                    <option value="1">Yes</option>
-                </select>
-                <button type="submit">Filtra</button>
-            </form>
-        </div>
-        <div class="row">
-            <form action="./index.php" method="get">
-                <label for="vote">How many stars should the hotel have?</label>
-                <input type="text" name="vote" id="vote">
-                <button type="submit">Filtra</button>
+                <div>
+                    <label for="parking">Do you need a parking?</label>
+                    <select name="parking" id="parking">
+                        <!-- di base il no è quello selezionato -->
+                        <option value="0" selected>Not</option>
+                        <option value="1">Yes</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="stars">How many stars should the hotel have, at least?</label>
+                    <input type="number" name="stars" id="stars" min="1" max="5">
+                </div>
+                <div>
+                    <button type="submit">Filtra</button>
+                </div>
             </form>
         </div>
         <div class="row">
@@ -127,7 +146,7 @@ if (isset($_GET["parking"])){
                 </thead>
                 <tbody>
                     <!-- il tr va istanziato per ogni hotel -->
-                    <?php foreach($hotels as $hotel) { ?>
+                    <?php foreach($filteredHotels as $hotel) { ?>
                         <tr>
                             <!-- per ogni proprietà di un hotel -->
                             <?php foreach ($hotel as $property) { ?>
